2c66554390c37850a13fed67ee426aae7cc3aee3
Maillait <williamottenbreit@gmail.com>
2025-07-24 10:38:23 -0600

    Update Dungeon.js

--- Dungeon.js
+++ Dungeon.js
@@ -2,27 +2,92 @@ let Px = 0;
 let Py = 0;
 let screen = [];
 let random = [];
-let enemyt = [];
-let enemyx = [];
-let enemyy = [];
+let enemy = [];
+let attack = []; // > 3? punch   > 7? basic gun   > 15? basic magic...
 const plot = document.getElementById("plot");
 const ctx = plot.getContext("2d");
 
-for (let i = 0; i < 5; i++) {
-  random.push(Math.floor(Math.random() * 3));
+let u = 0;
+let d = 0;
+let l = 0;
+let r = 0;
+let a1 = 0;
+let a2 = 0;
+
+let error = 0;
+
+drawDungeon();
+let tick = setInterval(step, 100);
+
+for (let i = 0; i < 100; i++) {
+  random.push(Math.floor(Math.random() * 2));
 }
 
 document.addEventListener("keydown", (event) => {
-  if (event.key == "ArrowUp" && !screen[((Py - 1 + 200) % 200) * 200 + Px]) Py--;
-  if (event.key == "ArrowDown" && !screen[((Py + 1) % 200) * 200 + Px]) Py++;
-  if (event.key == "ArrowRight" && !screen[Py * 200 + ((Px + 1) % 200)]) Px++;
-  if (event.key == "ArrowLeft" && !screen[Py * 200 + ((Px - 1 + 200) % 200)]) Px--;
+  if (event.key == "ArrowUp") u = 1;
+  if (event.key == "ArrowDown") d = 1;
+  if (event.key == "ArrowRight") r = 1;
+  if (event.key == "ArrowLeft") l = 1;
+  if (event.key == "1") a1 = 1;
+  if (event.key == "2") a2 = 1;
+});
+
+document.addEventListener("keyup", (event) => {
+  if (event.key == "ArrowUp") u = 0;
+  if (event.key == "ArrowDown") d = 0;
+  if (event.key == "ArrowRight") r = 0;
+  if (event.key == "ArrowLeft") l = 0;
+  if (event.key == "1") a1 = 0;
+  if (event.key == "2") a2 = 0;
+});
+
+function step() {
+  while (error == 1) {
+    error = 0;
+    drawDungeon();
+  }
+  let tenemy = [];
+  let change = 0;
+  if (u && !screen[((Py - 1 + 200) % 200) * 200 + Px]) { Py--;
+    change++;
+    for (let x = 0; x < 25; x++) {
+      for (let y = 0; y < 25; y++) {
+        if (y) {
+          tenemy[y * 25 + x] = enemy[(y - 1) * 25 + x];
+        } else {
+          tenemy[y * 25 + x] = 0;
+        }
+      }
+    }
+  }
+  if (d && !screen[((Py + 1) % 200) * 200 + Px]) {
+    Py++;
+    change++;
+    for (let x = 0; x < 25; x++) {
+      for (let y = 0; y < 25; y++) {
+        if (y < 24) {
+          tenemy[y * 25 + x] = enemy[(y + 1) * 25 + x];
+        } else {
+          tenemy[y * 25 + x] = 0;
+        }
+      }
+    }
+  }
+  if (r && !screen[Py * 200 + ((Px + 1) % 200)]) Px++;
+  if (l && !screen[Py * 200 + ((Px - 1 + 200) % 200)]) Px--;
   if (Py < 0) Py += 200;
   if (Px < 0) Px += 200;
   if (Py >= 200) Py -= 200;
   if (Px >= 200) Px -= 200;
+  if (change) {
+  for (let x = 0; x < 25; x++) {
+    for (let y = 0; y < 25; y++) {
+      enemy[y * 25 + x] = tenemy[y * 25 + x];
+    }
+  }
+}
   draw();
-});
+}
 
 function drawDungeon() {
   let rx = [];
@@ -82,6 +147,27 @@ function drawDungeon() {
       }
     }
   }
+  while (screen[Py * 200 + Px] && Px < 199) {
+    Px++;
+    if (Px > 100) {
+      Py++;
+    }
+    if (Px < 199) error = 1;
+  }
+  for (let x = 0; x < 25; x++) {
+    for (let y = 0; y < 25; y++) {
+      attack[y * 25 + x] = 0;
+      if (
+        (y == 0 || y == 24 || x == 0 || x == 24) &&
+        Math.random() < 1 &&
+        !screen[(y + Py - 10) * 200 + (x + Px - 10)]
+      ) {
+        enemy[y * 25 + x] = 1;
+      } else {
+        enemy[y * 25 + x] = 0;
+      }
+    }
+  }
 }
 
 function draw() {
@@ -89,29 +175,26 @@ function draw() {
   for (var y = 0; y < 20; y++) {
     for (var x = 0; x < 20; x++) {
       if (y == 10 && x == 10) {
-        ctx.fillStyle = "red";
+        ctx.fillStyle = "#a10b00ff";
+      } else if (enemy[(y + 3) * 25 + (x + 3)]) {
+        ctx.fillStyle = "#c2af09ff";
       } else if (
         screen[((y + Py + 190) % 200) * 200 + ((x + Px + 190) % 200)]
       ) {
-        if (
-          Math.round(
-            (Math.sin(y + Py) + Math.sin(x + Px) + random[Math.floor(Math.sin(y + Py) + Math.sin(x + Px) + 2)] +
-              2.9) /
-              6
-          )
-        ) {
+        if (Math.round(random[((x + Px) % 10) + ((Py + y) % 10) * 10])) {
           ctx.fillStyle = "#444444ff";
         } else {
           ctx.fillStyle = "#313830ff";
         }
       } else {
-        ctx.fillStyle = "#93a78aff";
+        if (Math.round(random[((x + Px) % 10) + ((Py + y) % 10) * 10])) {
+          ctx.fillStyle = "#898b8aff";
+        } else {
+          ctx.fillStyle = "#869695ff";
+        }
       }
       ctx.fillRect(20 * x, 20 * y, 20, 20);
     }
   }
   ctx.closePath();
 }
-
-drawDungeon();
-draw();
