b4e56c7be67642696977fa58066ad0f732da067b
Maillait <williamottenbreit@gmail.com>
2025-07-25 10:08:17 -0600

    Update Dungeon.js

--- Dungeon.js
+++ Dungeon.js
@@ -15,7 +15,11 @@ let a1 = 0;
 let a2 = 0;
 
 let error = 0;
-let spawn = 20;
+let spawn = 40;
+let artifacts = 0;
+let health = 100;
+let upgrade = 0;
+let explored = [];
 
 drawDungeon();
 let tick = setInterval(step, 100);
@@ -43,7 +47,7 @@ document.addEventListener("keyup", (event) => {
 });
 
 function step() {
-function change() {
+  function change() {
     for (let x = 0; x < 25; x++) {
       for (let y = 0; y < 25; y++) {
         enemy[y * 25 + x] = tenemy[y * 25 + x];
@@ -55,7 +59,8 @@ function change() {
     drawDungeon();
   }
   let tenemy = [];
-  if (u && !screen[((Py - 1 + 200) % 200) * 200 + Px]) {
+  ctx.beginPath();
+  if (u && 1 != screen[((Py - 1 + 200) % 200) * 200 + Px]) {
     Py--;
     for (let x = 0; x < 25; x++) {
       for (let y = 0; y < 25; y++) {
@@ -64,17 +69,38 @@ function change() {
         } else if (
           y == 0 &&
           Math.random() < 1 / spawn &&
-          !screen[((y + Py + 187) % 200) * 200 + ((x + Px + 187) % 200)]
+          1 != screen[((y + Py + 187) % 200) * 200 + ((x + Px + 187) % 200)]
         ) {
           tenemy[y * 25 + x] = 1;
         } else {
           tenemy[y * 25 + x] = 0;
         }
+        if (
+          1 != screen[((y + Py + 190) % 200) * 200 + ((x + Px + 190) % 200)]
+        ) {
+          if (x < 22 && x > 2 && y < 22 && y > 2) {
+            ctx.fillStyle = "rgba(24, 112, 43, 1)";
+            ctx.fillRect(
+              ((x + Px + 190) % 200) + 400,
+              (y + Py + 190) % 200,
+              1,
+              1
+            );
+          } else if (y > 22) {
+            ctx.fillStyle = "#9bb1c4ff";
+            ctx.fillRect(
+              ((x + Px + 190) % 200) + 400,
+              (y + Py + 190) % 200,
+              1,
+              1
+            );
+          }
+        }
       }
     }
     change();
   }
-  if (d && !screen[((Py + 1) % 200) * 200 + Px]) {
+  if (d && 1 != screen[((Py + 1) % 200) * 200 + Px]) {
     Py++;
     for (let x = 0; x < 25; x++) {
       for (let y = 0; y < 25; y++) {
@@ -83,17 +109,38 @@ function change() {
         } else if (
           y == 24 &&
           Math.random() < 1 / spawn &&
-          !screen[((y + Py + 187) % 200) * 200 + ((x + Px + 187) % 200)]
+          1 != screen[((y + Py + 187) % 200) * 200 + ((x + Px + 187) % 200)]
         ) {
           tenemy[y * 25 + x] = 1;
         } else {
           tenemy[y * 25 + x] = 0;
         }
+        if (
+          1 != screen[((y + Py + 190) % 200) * 200 + ((x + Px + 190) % 200)]
+        ) {
+          if (x < 22 && x > 2 && y < 22 && y > 2) {
+            ctx.fillStyle = "rgba(24, 112, 43, 1)";
+            ctx.fillRect(
+              ((x + Px + 190) % 200) + 400,
+              (y + Py + 190) % 200,
+              1,
+              1
+            );
+          } else if (y < 2) {
+            ctx.fillStyle = "#9bb1c4ff";
+            ctx.fillRect(
+              ((x + Px + 190) % 200) + 400,
+              (y + Py + 190) % 200,
+              1,
+              1
+            );
+          }
+        }
       }
     }
     change();
   }
-  if (r && !screen[Py * 200 + ((Px + 1) % 200)]) {
+  if (r && 1 != screen[Py * 200 + ((Px + 1) % 200)]) {
     Px++;
     for (let x = 0; x < 25; x++) {
       for (let y = 0; y < 25; y++) {
@@ -102,17 +149,38 @@ function change() {
         } else if (
           x == 24 &&
           Math.random() < 1 / spawn &&
-          !screen[((y + Py + 187) % 200) * 200 + ((x + Px + 187) % 200)]
+          1 != screen[((y + Py + 187) % 200) * 200 + ((x + Px + 187) % 200)]
         ) {
           tenemy[y * 25 + x] = 1;
         } else {
           tenemy[y * 25 + x] = 0;
         }
+        if (
+          1 != screen[((y + Py + 190) % 200) * 200 + ((x + Px + 190) % 200)]
+        ) {
+          if (x < 22 && x > 2 && y < 22 && y > 2) {
+            ctx.fillStyle = "rgba(24, 112, 43, 1)";
+            ctx.fillRect(
+              ((x + Px + 190) % 200) + 400,
+              (y + Py + 190) % 200,
+              1,
+              1
+            );
+          } else if (x < 2) {
+            ctx.fillStyle = "#9bb1c4ff";
+            ctx.fillRect(
+              ((x + Px + 190) % 200) + 400,
+              (y + Py + 190) % 200,
+              1,
+              1
+            );
+          }
+        }
       }
     }
     change();
   }
-  if (l && !screen[Py * 200 + ((Px - 1 + 200) % 200)]) {
+  if (l && 1 != screen[Py * 200 + ((Px - 1 + 200) % 200)]) {
     Px--;
     for (let x = 0; x < 25; x++) {
       for (let y = 0; y < 25; y++) {
@@ -121,20 +189,52 @@ function change() {
         } else if (
           x == 0 &&
           Math.random() < 1 / spawn &&
-          !screen[((y + Py + 187) % 200) * 200 + ((x + Px + 187) % 200)]
+          1 != screen[((y + Py + 187) % 200) * 200 + ((x + Px + 187) % 200)]
         ) {
           tenemy[y * 25 + x] = 1;
         } else {
           tenemy[y * 25 + x] = 0;
         }
+        if (
+          1 != screen[((y + Py + 190) % 200) * 200 + ((x + Px + 190) % 200)]
+        ) {
+          if (x < 22 && x > 2 && y < 22 && y > 2) {
+            ctx.fillStyle = "rgba(24, 112, 43, 1)";
+            ctx.fillRect(
+              ((x + Px + 190) % 200) + 400,
+              (y + Py + 190) % 200,
+              1,
+              1
+            );
+          } else if (x > 22) {
+            ctx.fillStyle = "#9bb1c4ff";
+            ctx.fillRect(
+              ((x + Px + 190) % 200) + 400,
+              (y + Py + 190) % 200,
+              1,
+              1
+            );
+          }
+        }
       }
     }
     change();
   }
+
   if (Py < 0) Py += 200;
   if (Px < 0) Px += 200;
   if (Py >= 200) Py -= 200;
   if (Px >= 200) Px -= 200;
+  if (screen[Py * 200 + Px] == 2) {
+    screen[Py * 200 + Px] = 0;
+    artifacts++;
+    ctx.beginPath();
+    ctx.fillStyle = "#695b4dff";
+    ctx.fillRect(422, 200, 200, 60);
+    ctx.fillStyle = "black";
+    ctx.fillText("Gems : " + artifacts, 430, 240);
+    ctx.closePath();
+  }
   draw();
 }
 
@@ -178,9 +278,9 @@ function drawDungeon() {
           );
         } else if (
           Math.round(
-            (screen[((y - 1 + 200) % 200) * 200 + x] +
+            (screen[((y + 199) % 200) * 200 + x] +
               screen[((y + 1) % 200) * 200 + x] +
-              screen[y * 200 + ((x - 1 + 200) % 200)] +
+              screen[y * 200 + ((x + 199) % 200)] +
               screen[y * 200 + ((x + 1) % 200)] +
               screen[y * 200 + x]) /
               5
@@ -196,7 +296,7 @@ function drawDungeon() {
       }
     }
   }
-  while (screen[Py * 200 + Px] && Px < 199) {
+  while (1 == screen[Py * 200 + Px] && Px < 199) {
     Px++;
     if (Px > 100) {
       Py++;
@@ -209,18 +309,50 @@ function drawDungeon() {
       enemy[y * 25 + x] = 0;
     }
   }
+  for (var y = 0; y < 200; y++) {
+    for (var x = 0; x < 200; x++) {
+      if (Math.random() < 1 / 200 && !screen[y * 200 + x]) {
+        screen[y * 200 + x] = 2;
+      }
+    }
+  }
+  ctx.beginPath();
+  ctx.imageSmoothingEnabled = false;
+  ctx.font = "bold 26px Papyrus";
+  ctx.fillStyle = "#695b4dff";
+  ctx.fillRect(400, 200, 200, 400);
+  ctx.fillStyle = "#000000ff";
+  ctx.fillRect(400, 0, 200, 200);
+  ctx.fillRect(400, 0, 1, 400);
+  ctx.fillText("Gems : " + artifacts, 430, 240);
+  ctx.fillRect(408, 226, 14, 14);
+  ctx.fillStyle = "#11d100ff";
+  ctx.fillRect(410, 228, 10, 10);
+  ctx.fillStyle = "#9bb1c48f";
+  ctx.fillRect(0, 0, 400, 400);
+  ctx.fillStyle = "rgba(24, 112, 43, 1)";
+  for (var y = 0; y < 20; y++) {
+    for (var x = 0; x < 20; x++) {
+      if (1 != screen[((y + Py + 190) % 200) * 200 + ((x + Px + 190) % 200)])
+        ctx.fillRect(((x + Px + 190) % 200) + 400, (y + Py + 190) % 200, 1, 1);
+    }
+  }
+  ctx.closePath();
 }
 
 function draw() {
-  ctx.beginPath();
   for (var y = 0; y < 20; y++) {
     for (var x = 0; x < 20; x++) {
       if (y == 10 && x == 10) {
         ctx.fillStyle = "#a10b00ff";
+      } else if (
+        screen[((y + Py + 190) % 200) * 200 + ((x + Px + 190) % 200)] == 2
+      ) {
+        ctx.fillStyle = "#11d100ff";
       } else if (enemy[(y + 3) * 25 + (x + 3)]) {
         ctx.fillStyle = "#c2af09ff";
       } else if (
-        screen[((y + Py + 190) % 200) * 200 + ((x + Px + 190) % 200)]
+        1 == screen[((y + Py + 190) % 200) * 200 + ((x + Px + 190) % 200)]
       ) {
         if (Math.round(random[((x + Px) % 10) + ((Py + y) % 10) * 10])) {
           ctx.fillStyle = "#444444ff";
